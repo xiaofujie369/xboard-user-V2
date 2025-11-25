dbdc45bb94eea4effeb983a52c1b1689fa84d244
xiaofujie369 <132354004+xiaofujie369@users.noreply.github.com>
2025-11-25 09:07:40 +0800

    Update env.example.js

--- env.example.js
+++ env.example.js
@@ -1,19 +1,24 @@
-// API地址
-window.routerBase = 'http://127.0.0.1:8000/'
+// API地址（线上环境）
+window.routerBase = 'https://ss.shy521.com/';
+
 window.settings = {
   // 站点名称
   title: 'Xboard',
   // 站点描述
   description: 'Xboard',
   assets_path: '/assets',
+
   // 主题色
   theme: {
-    color: 'default', //可选default、blue、black、、darkblue
+    color: 'default', // 可选 default、blue、black、darkblue
   },
+
   // 版本号
-  version: '0.1.1-dev',
-  // 登陆背景
+  version: '1.0.0',
+
+  // 登录背景图（你可填链接）
   background_url: '',
-  // 站点LOGO
+
+  // 站点LOGO（你可填链接）
   logo: '',
-}
+};
