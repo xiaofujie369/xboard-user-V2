8b48d519ec73aca7dddd79bc969271da4f624c14
xiaofujie369 <132354004+xiaofujie369@users.noreply.github.com>
2025-11-25 09:36:19 +0800

    Update env.example.js

--- env.example.js
+++ env.example.js
@@ -2,23 +2,11 @@
 window.routerBase = 'https://ss.shy521.com/';
 
 window.settings = {
-  // 站点名称
-  title: 'Xboard',
-  // 站点描述
-  description: 'Xboard',
+  title: '可云',
+  description: '可云',
   assets_path: '/assets',
-
-  // 主题色
-  theme: {
-    color: 'default', // 可选 default、blue、black、darkblue
-  },
-
-  // 版本号
+  theme: { color: 'default' },
   version: '1.0.0',
-
-  // 登录背景图（你可填链接）
   background_url: '',
-
-  // 站点LOGO（你可填链接）
   logo: '',
 };
